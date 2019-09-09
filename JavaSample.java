6ac56591fed71885d700631b5eaf07a549e87612
tsuyoshiikuno <easy.going.z244@gmail.com>
2019-09-09 14:22:07 +0900

    first commit

---
+++ JavaSample.java
@@ -0,0 +1,12 @@
+package JavaSample;
+
+public class JavaSample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello World!");
+		System.out.println("Hello Hiroshima!");
+		System.out.println("トベさん、こんにちは");
+	}
+
+}
